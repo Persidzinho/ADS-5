e39ca5000f758e5faeb2d00c1b67ed7673d33220
Никита Чванов <143615653+Persidzinho@users.noreply.github.com>
2026-04-04 19:33:40 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[size];
+    int topIndex;
+
+public:
+    TStack() {
+        topIndex = -1;
+    }
+
+    void push(T value) {
+        if (topIndex < size - 1) {
+            topIndex++;
+            arr[topIndex] = value;
+        }
+    }
+
+    T pop() {
+        if (topIndex >= 0) {
+            topIndex--;
+            return arr[topIndex + 1];
+        }
+        return T();
+    }
+
+    T get() const {
+        if (topIndex >= 0) {
+            return arr[topIndex];
+        }
+        return T();
+    }
+
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
